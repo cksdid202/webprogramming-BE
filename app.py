2fac1637f0697830f35189cd09deec162cc5ac34
20011744 <141496725+cksdid202@users.noreply.github.com>
2025-06-07 22:28:58 +0900

    feat:회원정보 수정, 탈퇴 구현

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["userdb"]
 user_col = db["users"]
 
+############################################################ user관련 
 # 회원가입
 @app.route('/api/register', methods=['POST'])
 def register():
@@ -66,5 +67,59 @@ def login():
         "name": user_data['name']
     }}), 200
 
+from bson.objectid import ObjectId  # ObjectId를 사용하여 _id로 찾기
+
+# 회원 정보 수정
+@app.route('/api/update', methods=['PUT'])
+def update_user():
+    data = request.json
+    user_id = data.get('user_id')  # 유저의 고유 _id
+    new_email = data.get('email')  # 이메일 수정 가능하도록 추가
+    new_password = data.get('password')
+    new_name = data.get('name')
+    new_phone = data.get('phone')
+
+    # 필수 입력 확인
+    if not all([user_id, new_password, new_name, new_phone]):
+        return jsonify({'message': '모든 필드를 입력해주세요.'}), 400
+
+    # 사용자 조회 (_id로 찾기)
+    user_data = user_col.find_one({"_id": ObjectId(user_id)})
+    if not user_data:
+        return jsonify({'message': '사용자를 찾을 수 없습니다.'}), 404
+    
+    # 중복 체크
+    if user_col.find_one({"email": new_email}):
+        return jsonify({'message': '이미 존재하는 이메일입니다.'}), 409
+
+    # 비밀번호 해싱
+    hashed_pw = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+
+    # 업데이트할 데이터 (이메일도 수정 가능하게 추가)
+    updated_data = {
+        "email": new_email,
+        "password": hashed_pw,
+        "name": new_name,
+        "phone": new_phone
+    }
+
+    # 사용자 정보 업데이트
+    user_col.update_one({"_id": ObjectId(user_id)}, {"$set": updated_data})
+
+    return jsonify({'message': '회원정보가 수정되었습니다.'}), 200
+
+
+
+############################################################## 여행지 관련
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
